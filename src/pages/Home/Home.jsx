f3959e9cfc43e36acd8a72e7f94a16966a68294d
unknown <bogdan.titsky@gmail.com>
2023-06-29 19:08:19 +0300

    safe

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from 'react';
 import { getMovies } from 'API';
 import { FilmList } from 'components/FilmList/FilmList';
+import { Loader } from 'components/Loader/Loader';
 
 export const Home = () => {
   const [films, setFilms] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchMovies = async () => {
       const response = await getMovies();
       const movies = response.results;
       setFilms(movies);
+
+      if (response.results !== 0) {
+        setError(false);
+        setIsLoading(false);
+      } else {
+        setError(true);
+        setIsLoading(false);
+      }
     };
 
     fetchMovies();
@@ -18,6 +29,9 @@ export const Home = () => {
   return (
     <main>
       <h1>Trending today</h1>
+
+      {isLoading && <Loader />}
+      {error && <p>Oops.. Simesing went wrong</p>}
       <FilmList films={films}></FilmList>
     </main>
   );
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -7,37 +7,32 @@ import { Notify } from 'notiflix';
 export const Movies = () => {
   const [searchInput, setSearchInput] = useState('');
   const [films, setFilms] = useState([]);
-
   const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
+    const getFilms = async query => {
+      const data = await getSearhedMovies(query);
+      setFilms(data.results);
+
+      if (data.results.length === 0) {
+        Notify.failure('нема таких фільмів, напиши шось інше');
+      }
+    };
+
     const searchValue = searchParams.get('query');
     if (searchValue) {
       getFilms(searchValue);
     }
-  }, []);
+  }, [searchParams]);
 
-  const getFilms = async query => {
-    const data = await getSearhedMovies(query);
-    setFilms(data.results);
-
-    if (data.results.length === 0 && searchInput) {
-      Notify.failure('нема таких фільмів, напиши шось інше');
-    }
-  };
-
-  const handleSubmit = async event => {
+  const handleSubmit = event => {
     event.preventDefault();
-    try {
-      if (!searchInput) {
-        return Notify.failure('Шо ти тут шукаєш???');
-      }
-      setSearchParams({ query: searchInput });
-      await getFilms(searchInput);
-      setSearchInput('');
-    } catch (error) {
-      console.error();
+
+    if (!searchInput) {
+      return Notify.failure('Шо ти тут шукаєш???');
     }
+    setSearchParams({ query: searchInput });
+    setSearchInput('');
   };
 
   return (
